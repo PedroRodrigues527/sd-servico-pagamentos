a416d90a2c8e44ddbdb125493a7575b509bfa3e8
PedroRodrigues527 <pedro.miguel.rodrigues00@gmail.com>
2023-10-31 10:03:50 +0000

    Routes for testing Unit test

--- api/routes/web.php
+++ api/routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/fail-route-test', function () {
+    return response()->json([
+        'message' => 'Erro test'], 400);
+});
+

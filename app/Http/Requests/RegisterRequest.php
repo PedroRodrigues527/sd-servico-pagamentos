984ed16104ac476ee5afe47c3dbeb7b2916cd424
PedroRodrigues527 <pedro.miguel.rodrigues00@gmail.com>
2023-11-28 19:51:48 +0000

    autenticação api

---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Password;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'username' => 'required|string|unique:users,username|max:255',
+            'password' => [
+                'required',
+                'string',
+                Password::min(8)->mixedCase()->numbers()->symbols()->uncompromised(),
+                'confirmed',
+            ]
+        ];
+    }
+}
